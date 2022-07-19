1395c7cd157a285f12016b34fbdc1f1e5c399f37
ashishjangid3055 <109598161+ashishjangid3055@users.noreply.github.com>
2022-07-19 22:16:38 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import Form from './components/Form'
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Profile from './components/Profile'
 import About from './components/About'
 import Interest from './components/Interest'
+import Social from './components/Social'
 
 function App() {
+  const [toggle, setToggle] = useState(false);
 
   return (
     <div className="App justify-content-center">
 
       <main className='col-md-4'>
-        <Profile />
+        <Profile toggle={toggle} setToggle={setToggle} />
         <About />
         <Interest /> 
+        <Social toggle={toggle} />  
       </main>
           
       <Form />
